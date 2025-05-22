d7c0dc06102bf9be17a67e11f265c2c309b487d8
nanduni13 <nandpathirana@gmail.com>
2025-05-22 01:57:46 +0000

    Main

---
+++ Main.java
@@ -0,0 +1,63 @@
+class Student {
+    private int studentId;
+    private String name;
+    private int daysAttended;
+
+    // Constructor
+    public Student(int studentId, String name, int daysAttended) {
+        this.studentId = studentId;
+        this.name = name;
+        this.daysAttended = daysAttended;
+    }
+
+    // Getters
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDaysAttended() {
+        return daysAttended;
+    }
+
+    // Setters
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDaysAttended(int daysAttended) {
+        this.daysAttended = daysAttended;
+    }
+
+    // Method 
+    public void displayDetails() {
+        System.out.println("ID: " + studentId + ", Name: " + name + ", Days Attended: " + daysAttended);
+    }
+}
+
+
+class Classroom {
+    private Student[] students = new Student[10];
+    private int count = 0;
+
+    
+    
+
+    
+    
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+        
+        classroom.displayAllStudents();
+    }
+}
